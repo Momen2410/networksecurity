256d46ffdeafa6e37b1c38cfde9aeda4ac9d654f
Momen2410 <momenwalied2002@gmail.com>
2025-01-24 20:00:24 +0200

    Data validation stage completed

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from networksecurity.componants.data_ingestion import DataIngestion
+from networksecurity.componants.data_validation import DataValidation
 from networksecurity.exceptions.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.ConfigEntity import DataIngestionConfig, TrainingPipelineConfig
+from networksecurity.entity.ConfigEntity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig
 import sys
 
 
@@ -10,8 +11,16 @@ if __name__ == '__main__':
     try:
         training_pipeline_config = TrainingPipelineConfig()
         data_ingestion_config = DataIngestionConfig(training_pipeline_config)
+        data_validation_config = DataValidationConfig(training_pipeline_config)
         data_ingestion = DataIngestion(data_ingestion_config)
+        logging.info("Initiate the data ingestion")
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+        logging.info("Data ingestion succeeded")
         print(data_ingestion_artifact)
+        data_validation=DataValidation(data_validation_config, data_ingestion_artifact)
+        logging.info("Initiate Data validation")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        print(data_validation_artifact)
+        logging.info("Data Validation succeeded")
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -1,13 +1,8 @@
 import os
-import sys
-import numpy as np
-import pandas as pd
-
 
 '''
 define common constant variables for training pipeline
 '''
-
 TARGET_COULMNS: str = 'Result'
 PIPLINE_NAME: str = 'NetworkSecurity'
 ARTIFACT_DIR: str = 'Artifact'
@@ -15,6 +10,7 @@ FILE_NAME: str = 'phisingData.csv'
 
 TRAIN_FILE_NAME: str = 'train.csv'
 TEST_FILE_NAME: str = 'test.csv'
+SCHEMA_FILE_PATH = os.path.join('data_schema', 'schema.yaml')
 
 '''
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
@@ -26,3 +22,12 @@ DATA_INGESTION_FEATURE_STORE_DIR: str = 'feature_store'
 DATA_INGESTION_INGESTED_DIR: str = 'ingested'
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
 
+"""
+Data Validation related constant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
